517a00a3155e3c87242094b5ec4d830cad66d7ae
Kai Davenport <kaiyadavenport@gmail.com>
2018-02-28 13:40:09 +0000

    #310: install dep as part of installing vagrant

--- scripts/install_vagrant.sh
+++ scripts/install_vagrant.sh
@@ -36,3 +36,6 @@ chmod +x /usr/local/bin/docker-compose
 curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
 chmod +x ./kubectl
 sudo mv ./kubectl /usr/local/bin/kubectl
+
+curl -L -o /usr/local/bin/dep https://github.com/golang/dep/releases/download/v0.4.1/dep-linux-amd64
+chmod a+x /usr/local/bin/dep
