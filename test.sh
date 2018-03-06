3266a43db7f230ed7cdceca178c21e72a2be0591
Luke Marsden <me@lukemarsden.net>
2018-03-06 17:55:41 +0000

    FIX: bash is hard to read.

--- test.sh
+++ test.sh
@@ -2,10 +2,11 @@
 # Run with arguments you want to pass to test.
 # Example: ./test.sh -run TestTwoSingleNodeClusters
 set -xe
+set -o pipefail
 echo "=== CONTAINERS RUNNING AT START OF TEST ==="
 docker ps
 echo "==========================================="
 export PATH=/usr/local/go/bin:$PATH
 export DISABLE_LOG_AGGREGATION=1
 cd tests
-mispipe "sudo -E `which go` test -v $@ 2>&1" ts
+(sudo -E `which go` test -v "$@" 2>&1 ) | ts
