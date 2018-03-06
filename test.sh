f5ab9e32a51797a895d971dd8a3533de9c5e4f7e
Luke Marsden <me@lukemarsden.net>
2018-03-06 08:14:20 +0000

    ops#15 log which containers are running when we start so that we can try to find correlations

--- test.sh
+++ test.sh
@@ -2,6 +2,9 @@
 # Run with arguments you want to pass to test.
 # Example: ./test.sh -run TestTwoSingleNodeClusters
 set -xe
+echo "=== CONTAINERS RUNNING AT START OF TEST ==="
+docker ps
+echo "==========================================="
 export PATH=/usr/local/go/bin:$PATH
 export DISABLE_LOG_AGGREGATION=1
 cd tests
