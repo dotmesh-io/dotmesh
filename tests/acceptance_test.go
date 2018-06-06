ad4f7af2a17a525e0da5e1b8b6309a71d55331b8
Charlotte Godley <charlotte@dotmesh.com>
2018-06-06 16:52:00 +0000

    #428: add test to confirm you can pass in a port now

--- tests/acceptance_test.go
+++ tests/acceptance_test.go
@@ -1346,6 +1346,10 @@ func TestTwoSingleNodeClusters(t *testing.T) {
 	node1 := f[0].GetNode(0).Container
 	node2 := f[1].GetNode(0).Container
 
+	t.Run("SpecifyPort", func(t *testing.T) {
+		citools.RunOnNode(t, node1, "echo FAKEAPIKEY | dm remote add funny_port_remote admin@"+f[1].GetNode(0).IP+":32607")
+	})
+
 	t.Run("PushCommitBranchExtantBase", func(t *testing.T) {
 		fsname := citools.UniqName()
 		citools.RunOnNode(t, node2, citools.DockerRun(fsname)+" touch /foo/X")
