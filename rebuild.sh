2ac3827f5be4f7738389ef76e7c1142eb0e256d4
Alaric Snell-Pym <alaric@dotmesh.com>
2018-07-11 12:49:40 +0100

    NFC: Moved operator, flexvolume, and provisioner out of the dotmesh-server directory

--- rebuild.sh
+++ rebuild.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 set -xe
+
+. build_setup.sh
+
 (cd cmd/dm && ./rebuild.sh Linux)
+if [ -z "${SKIP_K8S}" ]; then
+    ./rebuild_flexvolume.sh
+    ./rebuild_operator.sh
+fi
 ./rebuild_server.sh
 (cd kubernetes && ./rebuild.sh)
---
+++ rebuild_provisioner.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+. build_setup.sh
+
+# dm-provisioner
+echo "creating container: dotmesh-builder-dm-provisioner-$ARTEFACT_CONTAINER"
+docker rm -f dotmesh-builder-dm-provisioner-$ARTEFACT_CONTAINER || true
+docker run \
+       --name dotmesh-builder-dm-provisioner-$ARTEFACT_CONTAINER \
+       -v dotmesh_build_cache_dm_provisioner:/gocache \
+       -e GOPATH=/go -e GOCACHE=/gocache \
+       -e CGO_ENABLED=0 \
+       -w /go/src/github.com/dotmesh-io/dotmesh/cmd/dynamic-provisioning \
+       dotmesh-builder:$ARTEFACT_CONTAINER \
+       go build -pkgdir /go/pkg -ldflags '-extldflags "-static"' -o /target/dm-provisioner .
+echo "copy binary: /target/dm-provisioner"
+docker cp dotmesh-builder-dm-provisioner-$ARTEFACT_CONTAINER:/target/dm-provisioner target/
+docker rm -f dotmesh-builder-dm-provisioner-$ARTEFACT_CONTAINER
+
+echo "building image: ${CI_DOCKER_PROVISIONER_IMAGE}"
+docker build -f cmd/dynamic-provisioning/Dockerfile -t "${CI_DOCKER_PROVISIONER_IMAGE}" .
