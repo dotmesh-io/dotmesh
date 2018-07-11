4d6908355f8e44b2dd5ba4ac5d7aca4b0a1c7d86
Alaric Snell-Pym <alaric@dotmesh.com>
2018-07-11 13:46:29 +0100

    FIX: Typo

--- rebuild_provisioner.sh
+++ rebuild_provisioner.sh
@@ -10,7 +10,7 @@ docker run \
        -v dotmesh_build_cache_dm_provisioner:/gocache \
        -e GOPATH=/go -e GOCACHE=/gocache \
        -e CGO_ENABLED=0 \
-       -w /go/src/github.com/dotmesh-io/dotmesh/cmd/dynamic-provisioning \
+       -w /go/src/github.com/dotmesh-io/dotmesh/cmd/dynamic-provisioner \
        dotmesh-builder:$ARTEFACT_CONTAINER \
        go build -pkgdir /go/pkg -ldflags '-extldflags "-static"' -o /target/dm-provisioner .
 echo "copy binary: /target/dm-provisioner"
@@ -18,4 +18,4 @@ docker cp dotmesh-builder-dm-provisioner-$ARTEFACT_CONTAINER:/target/dm-provisio
 docker rm -f dotmesh-builder-dm-provisioner-$ARTEFACT_CONTAINER
 
 echo "building image: ${CI_DOCKER_PROVISIONER_IMAGE}"
-docker build -f cmd/dynamic-provisioning/Dockerfile -t "${CI_DOCKER_PROVISIONER_IMAGE}" .
+docker build -f cmd/dynamic-provisioner/Dockerfile -t "${CI_DOCKER_PROVISIONER_IMAGE}" .
