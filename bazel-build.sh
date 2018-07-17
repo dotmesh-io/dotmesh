2d838af3f6203851018f2527c84ab308289dbfb2
Charlotte Godley <charlotte@dotmesh.com>
2018-07-17 18:15:54 +0100

    #467: helper script for building all the things using bazel

---
+++ bazel-build.sh
@@ -0,0 +1,5 @@
+bazel build //cmd/dm:dm
+bazel build //cmd/dotmesh-server:dotmesh-server
+bazel build //cmd/dynamic-provisioner:dynamic-provisioner
+bazel build //cmd/flexvolume:flexvolume
+bazel build //cmd/operator:operator
