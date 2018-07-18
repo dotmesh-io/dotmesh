50e723a4472b44edaabb9144d7a5980e3bde682f
Priya Samuel <priya@dotmesh.io>
2018-07-18 14:58:17 +0000

    NFC: dep wrangling, remove duplicate deps between dotmesh/ and tests/

--- tests/vendor/github.com/dotmesh-io/citools/apply-patch.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-# Generate dind-cluster-patched.sh from dind-cluster-original.sh by applying dind-cluster.sh.patch
-patch -o dind-cluster-patched.sh dind-cluster-original.sh dind-cluster.sh.patch
--- tests/vendor/github.com/dotmesh-io/citools/update-patch.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-# Generate dind-cluster.sh.patch from the differences between dind-cluster-original.sh and dind-cluster-patched.sh
-
-diff -u dind-cluster-original.sh dind-cluster-patched.sh > dind-cluster.sh.patch
