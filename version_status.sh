8e6a1a365910e807bdcf263fae604134754be360
Charlotte Godley <charlotte@dotmesh.com>
2018-07-19 16:19:36 +0100

    NFC: fiddling with paths

--- version_status.sh
+++ version_status.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # script to tell bazel what version to set on the client
-VERSION=$(cd cmd/dm/versioner && go run versioner.go)
+VERSION=$(cd cmd/versioner && go run versioner.go)
 echo VERSION ${VERSION}
 echo DOCKERTAG ${CI_DOCKER_TAG}
 echo CI_REGISTRY ${CI_DOCKER_REGISTRY:-$(hostname).local:80}
