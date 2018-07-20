c2430e8c1dab825ee108c37b0550b97dfb18ed7d
Charlotte Godley <charlotte@dotmesh.com>
2018-07-20 11:25:37 +0100

    #467: manage cross compiling paths

--- rebuild_client.sh
+++ rebuild_client.sh
@@ -5,15 +5,15 @@ set -ex
 OS=$1
 if [ $OS = "Linux" ]; then 
     platform="linux_amd64"
-    output_dir=${platform}_stripped
 elif [ $OS = "Darwin" ]; then 
     platform="darwin_amd64" 
-    output_dir=${platform}_stripped
 else
     echo "Please enter Linux or Darwin as the first arg"
     exit 1
 fi
 
+location=$(realpath .)/bazel-bin/cmd
+output_dir=${platform}_stripped
 mkdir -p target/$OS/
 
 if [ x$CI_DOCKER_TAG == x ]
@@ -23,6 +23,11 @@ then
 fi
 
 
-location=$(realpath .)/bazel-bin/cmd
+
+path=${location}/dm/$output_dir/dm
 bazel build //cmd/dm:dm --platforms=@io_bazel_rules_go//go/toolchain:$platform --workspace_status_command=$(realpath ./version_status.sh)
+# tiny bit hacky - if we're on a mac and compiling for linux the output will be "pure", and vice versa compiling for mac from linux
+if [ ! -d "${location}/dm/${output_dir}" ]; then
+    output_dir=${platform}_pure_stripped
+fi
 cp ${location}/dm/$output_dir/dm target/$OS/
