249fa6962f278d69e74226d9d1463c8dfe07a146
Charlotte <charlotte@dotmesh.com>
2018-07-23 14:26:54 +0000

    #467: make release script match changes in version status

--- release-images.sh
+++ release-images.sh
@@ -4,7 +4,7 @@ set -xe
 source lib.sh
 
 main() {
-    export CI_DOCKER_REGISTRY=$1
+    export CI_REGISTRY=$1
     bazel-with-workspace run //cmd/dotmesh-server:dotmesh-server_push
     bazel-with-workspace run //cmd/operator:operator_push
     bazel-with-workspace run //cmd/dynamic-provisioner:provisioner_push
