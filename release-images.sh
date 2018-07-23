187843224a91d7bdb8feb19c953615bdcb9e735b
Charlotte Godley <charlotte@dotmesh.com>
2018-07-23 10:29:19 +0100

    #467: refactor release to quay to be more bazel-y

---
+++ release-images.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+set -xe
+
+source lib.sh
+
+main() {
+    export CI_DOCKER_REGISTRY=$1
+    bazel-with-workspace run //cmd/dotmesh-server:dotmesh-server_push
+    bazel-with-workspace run //cmd/operator:operator_push
+    bazel-with-workspace run //cmd/dynamic-provisioner:provisioner_push
+}
+
+main
