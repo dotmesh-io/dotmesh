0fd064b767eac26ca5444d5e38eb208abdda61b7
Alaric Snell-Pym <alaric@dotmesh.com>
2018-07-24 14:30:13 +0100

    FIX: Better integrate bazel-free build, automatically branching to it on NixOS so I can still use ./prep-tests.sh like normal people

--- rebuild.sh
+++ rebuild.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 set -xe
 
+if [ -f /etc/NIXOS ]
+then
+    # Bazel doesn't work well with NixOS, so use plan B.
+    exec ./rebuild_in_container_without_bazel.sh "$@"
+fi
+
 source build-lib.sh
 
 main() {
