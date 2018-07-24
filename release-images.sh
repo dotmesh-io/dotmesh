6514ecb291051831b6bbd62bd66ee95eab709991
Alaric Snell-Pym <alaric@dotmesh.com>
2018-07-24 17:23:18 +0100

    #496: Get Bazel to push to the new location

--- release-images.sh
+++ release-images.sh
@@ -5,6 +5,7 @@ source lib.sh
 
 main() {
     export CI_REGISTRY=$1
+    export CI_REPOSITORY=$2
     bazel-with-workspace run //cmd/dotmesh-server:dotmesh-server_push
     # do a full rebuild on operator because it needs to know the server image link
     build-operator
