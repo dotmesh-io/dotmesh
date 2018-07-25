8caa45a24d9e01f20fc2d57e56dc1faeee29cf8c
Luke Marsden <me@lukemarsden.net>
2018-07-25 09:23:40 +0100

    FIX: https://github.com/lxc/lxc/issues/1858#issuecomment-338241969 suggests canmount=noauto might help us

--- cmd/dotmesh-server/mount.go
+++ cmd/dotmesh-server/mount.go
@@ -42,6 +42,14 @@ func (f *fsMachine) mountSnap(snapId string, readonly bool) (responseEvent *Even
 		if readonly {
 			options += ",ro"
 		}
+		logZFSCommand(fullId, fmt.Sprintf("zfs set canmount=noauto %s", zfsPath))
+		out, err := exec.Command("zfs", "set", "canmount=noauto", zfsPath).CombinedOutput()
+		if err != nil {
+			return &Event{
+				Name: "failed-settings-canmount-noauto",
+				Args: &EventArgs{"err": err, "out": out, "zfsPath", zfsPath},
+			}
+		}
 		logZFSCommand(fullId, fmt.Sprintf("mount.zfs -o %s %s %s", options, zfsPath, mountPath))
 		out, err := exec.Command("mount.zfs", "-o", options,
 			zfsPath, mountPath).CombinedOutput()
