bd9124584e454b732c5e150e9804c7c8417fcfce
Priya Samuel <priya@dotmesh.io>
2018-07-27 22:01:52 +0000

    #463: Remove zpool clean up logic from require_zfs.sh

--- cmd/dotmesh-server/require_zfs.sh
+++ cmd/dotmesh-server/require_zfs.sh
@@ -343,26 +343,6 @@ cleanup() {
         run_in_zfs_container inspect-namespace /bin/cat /proc/self/mountinfo | sed 's/^/INNER: /' || true
         echo "DEBUG End of mount tables."
     fi
-
-    # Release the ZFS pool. Do so in a mount namespace which has $OUTER_DIR
-    # rshared, otherwise zpool export's unmounts can be mighty confusing.
-
-    # Step 1: Unmount any dots and the ZFS mountpoint (if it's there)
-    echo "Unmount dots in $MOUNTPOINT/mnt/dmfs:"
-    run_in_zfs_container zpool-unmount-dots sh -c "cd \"$MOUNTPOINT/mnt/dmfs\" && for i in *; do umount --force \$i; done" || true
-    echo "Unmounting $MOUNTPOINT:"
-    run_in_zfs_container zpool-unmount umount --force --recursive "$MOUNTPOINT"|| true
-
-    # Step 2: Shut down the pool.
-    echo "zpool exporting $POOL:"
-    if run_in_zfs_container zpool-export zpool export -f "$POOL"
-    then
-        echo "`date`: Exported pool OK" >> $POOL_LOGFILE
-        echo "Exported pool OK."
-    else
-        echo "`date`: ERROR: Failed exporting pool!" >> $POOL_LOGFILE
-        echo "ERROR: Failed exporting pool!."
-    fi
 }
 
 shutdown() {
