3a712aba97e422e45c9f1df6e08c3aaf35faa1dc
Alaric Snell-Pym <alaric@dotmesh.com>
2018-07-31 16:31:54 +0100

    #515: Bump citools to see how long we retry for during healthy startup

--- tests/vendor/github.com/dotmesh-io/citools/testtools.go
+++ tests/vendor/github.com/dotmesh-io/citools/testtools.go
@@ -1720,7 +1720,8 @@ func (c *Kubernetes) Start(t *testing.T, now int64, i int) error {
 	// Add the nodes at the end, because NodeFromNodeName expects dotmesh
 	// config to be set up.
 	for j := 0; j < c.DesiredNodeCount; j++ {
-		for {
+		dotmeshIteration := 0
+		for ; ; dotmeshIteration++ {
 			st, err = docker(
 				nodeName(now, i, j),
 				"echo FAKEAPIKEY | dm remote add local admin@127.0.0.1",
@@ -1758,6 +1759,7 @@ func (c *Kubernetes) Start(t *testing.T, now int64, i int) error {
 				break
 			}
 		}
+		fmt.Printf("RETRIES: dotmesh started on cluster %d node %d after %d tries\n", i, j, dotmeshIteration)
 		c.Nodes[j] = NodeFromNodeName(t, now, i, j, clusterName)
 	}
 
@@ -1765,7 +1767,8 @@ func (c *Kubernetes) Start(t *testing.T, now int64, i int) error {
 	// around https://github.com/dotmesh-io/dotmesh/issues/62 so
 	// removing this will be a good test of that issue :-)
 	fmt.Printf("Waiting for etcd...\n")
-	for {
+	etcdIteration := 0
+	for ; ; etcdIteration++ {
 		resp := OutputFromRunOnNode(t, c.Nodes[0].Container, "kubectl describe etcd dotmesh-etcd-cluster -n dotmesh | grep Type:")
 		if err != nil {
 			return err
@@ -1785,6 +1788,7 @@ func (c *Kubernetes) Start(t *testing.T, now int64, i int) error {
 			return err
 		}
 	}
+	fmt.Printf("RETRIES: etcd started after %d tries\n", etcdIteration)
 
 	// For each node, wait until we can talk to dm from that node before
 	// proceeding.
