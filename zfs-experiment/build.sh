1d3b84eb946340c94b104dbb3258bed310a09477
Luke Marsden <me@lukemarsden.net>
2018-08-01 08:21:29 +0100

    #440: script to debootstrap and install debs in a prefix

--- zfs-experiment/build.sh
+++ zfs-experiment/build.sh
@@ -1,7 +1,25 @@
 #!/bin/sh
-
+set -xe
 mkdir -p outputs/zfs-{0.6,0.7}
 
-docker build -f Dockerfile.0-6 -t dotmesh/zfs-userland-0.6 .
-docker build -f Dockerfile.0-7 -t dotmesh/zfs-userland-0.7 .
+NAME=zfs-builder-$$
 
+docker build -f Dockerfile.0-6 -t dotmesh/zfs-userland-0.6 .
+#docker build -f Dockerfile.0-7 -t dotmesh/zfs-userland-0.7 .
+
+docker run --name $NAME -i --privileged dotmesh/zfs-userland-0.6 bash -c '
+    debootstrap artful /opt/zfs-0.6
+'
+
+docker commit $NAME dotmesh/zfs-userland-0.6-debootstrapped
+docker rm -f $NAME
+
+docker run --name $NAME -i --privileged dotmesh/zfs-userland-0.6-debootstrapped bash -c '
+    cd /var/cache && find . && \
+    groupadd crontab && groupadd messagebus && \
+    dpkg --root=/opt/zfs-0.6 -i /var/cache/apt/archives/python3-minimal*.deb
+    dpkg --root=/opt/zfs-0.6 -i /var/cache/apt/archives/*.deb
+'
+
+docker commit $NAME dotmesh/zfs-userland-0.6-installed
+docker rm -f $NAME
