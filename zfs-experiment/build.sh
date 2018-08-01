0ca5c142be92b56a5492998a9bda099f1d2182b8
Luke Marsden <me@lukemarsden.net>
2018-08-01 13:48:30 +0100

    #440: mixed userland zfs tools tarball generator

--- zfs-experiment/build.sh
+++ zfs-experiment/build.sh
@@ -1,25 +1,29 @@
 #!/bin/sh
 set -xe
-mkdir -p outputs/zfs-{0.6,0.7}
+mkdir -p outputs
 
 NAME=zfs-builder-$$
 
 docker build -f Dockerfile.0-6 -t dotmesh/zfs-userland-0.6 .
-#docker build -f Dockerfile.0-7 -t dotmesh/zfs-userland-0.7 .
+docker build -f Dockerfile.0-7 -t dotmesh/zfs-userland-0.7 .
 
-docker run --name $NAME -i --privileged dotmesh/zfs-userland-0.6 bash -c '
+docker run --rm --name $NAME -i --privileged -v ${PWD}/outputs:/outputs dotmesh/zfs-userland-0.6 bash -c '
     debootstrap artful /opt/zfs-0.6
-'
-
-docker commit $NAME dotmesh/zfs-userland-0.6-debootstrapped
-docker rm -f $NAME
-
-docker run --name $NAME -i --privileged dotmesh/zfs-userland-0.6-debootstrapped bash -c '
     cd /var/cache && find . && \
     groupadd crontab && groupadd messagebus && \
     dpkg --root=/opt/zfs-0.6 -i /var/cache/apt/archives/python3-minimal*.deb
     dpkg --root=/opt/zfs-0.6 -i /var/cache/apt/archives/*.deb
+    cd /opt/ && \
+    tar cf /outputs/zfs-0.6.tar zfs-0.6
 '
 
-docker commit $NAME dotmesh/zfs-userland-0.6-installed
-docker rm -f $NAME
+docker run --rm --name $NAME -i --privileged -v ${PWD}/outputs:/outputs dotmesh/zfs-userland-0.7 bash -c '
+    debootstrap bionic /opt/zfs-0.7
+    cd /var/cache && find . && \
+    groupadd crontab && groupadd messagebus && \
+    dpkg --root=/opt/zfs-0.7 -i /var/cache/apt/archives/libpython3*.deb
+    dpkg --root=/opt/zfs-0.7 -i /var/cache/apt/archives/python3-minimal*.deb
+    dpkg --root=/opt/zfs-0.7 -i /var/cache/apt/archives/*.deb
+    cd /opt/ && \
+    tar cf /outputs/zfs-0.7.tar zfs-0.7
+'
