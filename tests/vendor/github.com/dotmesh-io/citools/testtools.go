03bff71d0ae1773008c7a65d829cf7d90ddc8ede
Luke Marsden <me@lukemarsden.net>
2018-08-06 13:51:40 +0100

    #533: bump citools; make a kubeadm-dind-net network which covers the range of all possible clusters, not just the one we happen to create the first time when there is no network

--- tests/vendor/github.com/dotmesh-io/citools/testtools.go
+++ tests/vendor/github.com/dotmesh-io/citools/testtools.go
@@ -358,8 +358,8 @@ DNS_SERVICE="${DNS_SERVICE:-kube-dns}"
 					fi
 					(cd %s
 						EXTRA_DOCKER_ARGS="-v /dotmesh-test-pools:/dotmesh-test-pools:rshared -v /var/run/docker.sock:/hostdocker.sock %s " \
-						DIND_SUBNET="192.168.%d.0" \
-						DIND_SUBNET_SIZE="24" \
+						DIND_SUBNET="192.168.0.0" \
+						DIND_SUBNET_SIZE="16" \
 						SERVICE_CIDR="%s" \
 						POD_NETWORK_CIDR="%s" \
 						CNI_PLUGIN=bridge %s run $NODE "%s" %d)
@@ -394,9 +394,6 @@ DNS_SERVICE="${DNS_SERVICE:-kube-dns}"
 						'
 					fi
 					`, node, runScriptDir, mountDockerAuth,
-						// clusterIpPrefix is used here to generate a
-						// 192.168.x.0/24 DIND_SUBNET
-						clusterIpPrefix,
 						// clusterIpPrefix is used here to generate a
 						// SERVICE_CIDR...
 						serviceCIDR(clusterIpPrefix),
