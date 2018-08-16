fcf2fc138fd51b3db15209bb0ce9a3cc085696aa
Charlotte Godley <charlotte@dotmesh.com>
2018-08-16 13:53:25 +0000

    #122: corrected dm command

--- tests/s3api_test.go
+++ tests/s3api_test.go
@@ -25,7 +25,7 @@ func TestS3Api(t *testing.T) {
 
 	t.Run("Put", func(t *testing.T) {
 		dotName := citools.UniqName()
-		citools.RunOnNode(t, node1, "dm dot init "+dotName)
+		citools.RunOnNode(t, node1, "dm init "+dotName)
 		cmd := fmt.Sprintf("curl -T newfile.txt -u admin:%s 127.0.0.1:32607/s3/admin-%s/newfile", host.Password, dotName)
 		citools.RunOnNode(t, node1, "echo helloworld > newfile.txt")
 		citools.RunOnNode(t, node1, cmd)
