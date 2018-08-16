80fb30732cb8a240ff0da3dac4624ff79f44be05
Charlotte Godley <charlotte@dotmesh.com>
2018-08-16 11:37:11 +0100

    #122: tests for putting files

---
+++ tests/s3api_test.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"github.com/dotmesh-io/citools"
+	"strings"
+	"testing"
+)
+
+func TestS3Api(t *testing.T) {
+	citools.TeardownFinishedTestRuns()
+
+	f := citools.Federation{citools.NewCluster(1)}
+	defer citools.TestMarkForCleanup(f)
+	citools.AddFuncToCleanups(func() { citools.TestMarkForCleanup(f) })
+
+	citools.StartTiming()
+	err := f.Start(t)
+	if err != nil {
+		t.Fatal(err)
+	}
+	host := f[0].GetNode(0)
+	node1 := host.Container
+
+	// TODO not sure the s3 mock used here actually cares for authentication so this may not be enough. Also probably doesn't support versioning...
+	t.Run("Put", func(t *testing.T) {
+		dotName := citools.UniqName()
+		cmd := fmt.Sprintf("curl -T newfile.txt -u admin:%s %s:32607/s3/admin-%s/newfile", host.ApiKey, host.IP, dotName)
+		citools.RunOnNode(t, node1, "echo helloworld > newfile.txt")
+		citools.RunOnNode(t, node1, cmd)
+		resp := citools.OutputFromRunOnNode(t, node1, citools.DockerRun(dotName)+" ls /foo/")
+		if !strings.Contains(resp, "newfile.txt") {
+			t.Error("failed to create file")
+		}
+		resp = citools.OutputFromRunOnNode(t, node1, citools.DockerRun(dotName)+" cat /foo/newfile.txt")
+		if !strings.Contains(resp, "helloworld") {
+			t.Error("failed to upload file")
+		}
+	})
+}
