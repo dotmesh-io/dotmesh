4ccf3b8e0f54f9560be8688ef4bf52a559da9f5d
Charlotte Godley <charlotte@dotmesh.com>
2018-08-16 14:33:54 +0100

    #122: fixup tests with localhost as we're on the node already

--- tests/s3api_test.go
+++ tests/s3api_test.go
@@ -21,12 +21,12 @@ func TestS3Api(t *testing.T) {
 	}
 	host := f[0].GetNode(0)
 	node1 := host.Container
-	err = citools.RegisterUser(node1, "bob", "bob@bob.com", "password")
+	err = citools.RegisterUser(host, "bob", "bob@bob.com", "password")
 
 	t.Run("Put", func(t *testing.T) {
 		dotName := citools.UniqName()
 		citools.RunOnNode(t, node1, "dm dot init "+dotName)
-		cmd := fmt.Sprintf("curl -T newfile.txt -u admin:%s http://%s:32607/s3/admin-%s/newfile", host.Password, host.IP, dotName)
+		cmd := fmt.Sprintf("curl -T newfile.txt -u admin:%s 127.0.0.1:32607/s3/admin-%s/newfile", host.Password, dotName)
 		citools.RunOnNode(t, node1, "echo helloworld > newfile.txt")
 		citools.RunOnNode(t, node1, cmd)
 		resp := citools.OutputFromRunOnNode(t, node1, citools.DockerRun(dotName)+" ls /foo/")
@@ -41,7 +41,7 @@ func TestS3Api(t *testing.T) {
 
 	t.Run("PutDotDoesntExist", func(t *testing.T) {
 		dotName := citools.UniqName()
-		cmd := fmt.Sprintf("curl -T newfile.txt -u admin:%s %s:32607/s3/admin-%s/newfile", host.Password, host.IP, dotName)
+		cmd := fmt.Sprintf("curl -T newfile.txt -u admin:%s 127.0.0.1:32607/s3/admin-%s/newfile", host.Password, dotName)
 		citools.RunOnNode(t, node1, "echo helloworld > newfile.txt")
 		resp := citools.OutputFromRunOnNode(t, node1, cmd)
 		if !strings.Contains(resp, fmt.Sprintf("Bucket admin-%s does not exist", dotName)) {
@@ -51,7 +51,7 @@ func TestS3Api(t *testing.T) {
 
 	t.Run("PutUsernameMismatch", func(t *testing.T) {
 		dotName := citools.UniqName()
-		cmd := fmt.Sprintf("curl -T newfile.txt -u bob:password %s:32607/s3/admin-%s/newfile", host.IP, dotName)
+		cmd := fmt.Sprintf("curl -T newfile.txt -u bob:password 127.0.0.1:32607/s3/admin-%s/newfile", dotName)
 		citools.RunOnNode(t, node1, "echo helloworld > newfile.txt")
 		resp := citools.OutputFromRunOnNode(t, node1, cmd)
 		if !strings.Contains(resp, "User is not the administrator of namespace admin") {
