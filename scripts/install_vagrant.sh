3c471caeb1b642984dd576e7fe610433ed482f08
Charlotte Godley <charlotte@dotmesh.com>
2018-09-04 16:43:18 +0100

    NFC: shift bazel stuff into prepare
    don't want it to get ran as root

--- scripts/install_vagrant.sh
+++ scripts/install_vagrant.sh
@@ -11,8 +11,6 @@ apt-get install -y docker.io zfsutils-linux jq curl software-properties-common
 add-apt-repository -y ppa:gophers/archive
 apt-get -y update
 apt-get install -y golang-1.10 moreutils pkg-config zip g++ zlib1g-dev unzip python git-core
-curl -L -o bazel-installer.sh https://github.com/bazelbuild/bazel/releases/download/0.15.2/bazel-0.15.2-installer-linux-x86_64.sh
-chmod +x bazel-installer.sh && ./bazel-installer.sh --user
 
 # make elastic search work
 echo 'vm.max_map_count=262144' >> /etc/sysctl.conf
@@ -41,7 +39,3 @@ sudo mv ./kubectl /usr/local/bin/kubectl
 
 curl -L -o /usr/local/bin/dep https://github.com/golang/dep/releases/download/v0.4.1/dep-linux-amd64
 chmod a+x /usr/local/bin/dep
-
-apt-get -y install pkg-config zip g++ zlib1g-dev unzip python git-core
-curl -L -o bazel-installer.sh https://github.com/bazelbuild/bazel/releases/download/0.15.2/bazel-0.15.2-installer-linux-x86_64.sh
-chmod +x bazel-installer.sh && ./bazel-installer.sh --user
