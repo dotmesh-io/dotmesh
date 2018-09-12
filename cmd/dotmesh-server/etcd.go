173933440391aa0fc7c8086c6a98739b1c4b3cf9
Alaric Snell-Pym <alaric@dotmesh.com>
2018-09-12 14:37:43 +0100

    FIX: Just in case it helps, change the order we initialise things when first reading from etcd
    The masters were being updated quite low in the list, which may be breaking
    our handling of other things higher up the list - the masters cache is used
    as a validity check for dot IDs in various bits of the code!
    
    The new order is a thumb-in-the-air guess at likely dataflow dependencies
    between various cache update codes...

--- cmd/dotmesh-server/etcd.go
+++ cmd/dotmesh-server/etcd.go
@@ -1215,6 +1215,18 @@ func (s *InMemoryState) fetchAndWatchEtcd() error {
 			}
 		}
 	}
+
+	if masters != nil {
+		for _, node := range masters.Nodes {
+			modified := updateMine(node)
+			if modified {
+				if err = s.handleOneFilesystemMaster(node); err != nil {
+					return err
+				}
+			}
+		}
+	}
+
 	if registryFilesystems != nil {
 		for _, namespace := range registryFilesystems.Nodes {
 			for _, topLevelFilesystem := range namespace.Nodes {
@@ -1233,6 +1245,16 @@ func (s *InMemoryState) fetchAndWatchEtcd() error {
 			}
 		}
 	}
+	if serverSnapshots != nil {
+		for _, servers := range serverSnapshots.Nodes {
+			for _, filesystem := range servers.Nodes {
+				if err = updateSnapshots(filesystem); err != nil {
+					return err
+				}
+			}
+		}
+	}
+
 	if dirtyFilesystems != nil {
 		for _, filesystem := range dirtyFilesystems.Nodes {
 			for _, dirty := range filesystem.Nodes {
@@ -1255,25 +1277,6 @@ func (s *InMemoryState) fetchAndWatchEtcd() error {
 		for _, node := range interclusterTransfers.Nodes {
 			if err = updateTransfers(node); err != nil {
 				return err
-			}
-		}
-	}
-	if serverSnapshots != nil {
-		for _, servers := range serverSnapshots.Nodes {
-			for _, filesystem := range servers.Nodes {
-				if err = updateSnapshots(filesystem); err != nil {
-					return err
-				}
-			}
-		}
-	}
-	if masters != nil {
-		for _, node := range masters.Nodes {
-			modified := updateMine(node)
-			if modified {
-				if err = s.handleOneFilesystemMaster(node); err != nil {
-					return err
-				}
 			}
 		}
 	}
