8a096802ea0fc79227d185f41e85a5e6a50e2f03
Karolis Rusenas <karolis.rusenas@gmail.com>
2018-09-17 17:56:02 +0100

    gateway#32 don't create a new http client for each request

--- pkg/client/client.go
+++ pkg/client/client.go
@@ -84,9 +84,8 @@ func (j *JsonRpcClient) reallyCallRemote(
 
 	req.Header.Set("Content-Type", "application/json")
 	req.SetBasicAuth(j.User, j.ApiKey)
-	client := new(http.Client)
 
-	resp, err := client.Do(req)
+	resp, err := http.DefaultClient.Do(req)
 	if err != nil {
 		return err
 	}
