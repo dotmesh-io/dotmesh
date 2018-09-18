74bda100a0b97e35a22a6dabb61f58437ccc2362
Luke Marsden <me@lukemarsden.net>
2018-09-18 14:29:40 +0100

    #575: hard-code an anonymous user id

--- pkg/types/tlf.go
+++ pkg/types/tlf.go
@@ -10,6 +10,7 @@ import (
 
 // special admin user with global privs
 const ADMIN_USER_UUID = "00000000-0000-0000-0000-000000000000"
+const ANONYMOUS_USER_UUID = "FFFFFFFF-FFFF-FFFF-FFFF-FFFFFFFFFFFF"
 
 type TopLevelFilesystem struct {
 	MasterBranch  DotmeshVolume
