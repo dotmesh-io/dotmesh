c56f144b43e46fae82b8dca42d03ae5d00a3d401
Charlotte <charlotte@dotmesh.com>
2018-10-02 16:17:00 +0000

    dotscience-agent#88: refactor to avoid duplication and apply the logic to tosnaps

--- cmd/dotmesh-server/pushinitiatorstate.go
+++ cmd/dotmesh-server/pushinitiatorstate.go
@@ -407,6 +407,25 @@ func (f *fsMachine) push(
 	}, discoveringState
 }
 
+func stash(filesystemId, snapId string, client *dmclient.JsonRpcClient, ctx context.Context) (*Event, stateFn) {
+	var newBranch string
+	e := client.CallRemote(
+		ctx,
+		"DotmeshRPC.StashAfter",
+		types.StashRequest{
+			FilesystemId: filesystemId,
+			SnapshotId:   snapId,
+		},
+		&newBranch,
+	)
+	if e != nil {
+		return &Event{
+			Name: "failed-stashing-remote-end", Args: &EventArgs{"err": e},
+		}, backoffState
+	}
+	return nil, discoveringState
+}
+
 func (f *fsMachine) retryPush(
 	fromFilesystemId, fromSnapshotId, toFilesystemId, toSnapshotId string,
 	transferRequestId string, pollResult *TransferPollResult,
@@ -480,7 +499,6 @@ func (f *fsMachine) retryPush(
 				}, backoffState
 			}
 			snapRange, err := canApply(localSnaps, remoteSnaps)
-			returnErr := true
 			if err != nil {
 				switch err := err.(type) {
 				case *ToSnapsUpToDate:
@@ -497,46 +515,26 @@ func (f *fsMachine) retryPush(
 					return &Event{
 						Name: "peer-up-to-date",
 					}, backoffState
+				// here we tell the other end to get it's house in order, then return an error so we go round the loop again to get the commit list etc.
 				case *ToSnapsDiverged:
 					if transferRequest.StashDivergence {
-						returnErr = false
-						var newBranch string
-						e := client.CallRemote(
-							ctx,
-							"DotmeshRPC.StashAfter",
-							types.StashRequest{
-								FilesystemId: toFilesystemId,
-								SnapshotId:   err.latestCommonSnapshot.Id,
-							},
-							&newBranch,
-						)
-						if e != nil {
-							return &Event{
-								Name: "failed-stashing-remote-end", Args: &EventArgs{"err": e},
-							}, backoffState
+						event, state := stash(toFilesystemId, err.latestCommonSnapshot.Id, client, ctx)
+						if event != nil {
+							return event, state
 						}
-						// could also re-fetch the remote end commits here
-						remoteSnaps, e := restrictSnapshots(remoteSnaps, err.latestCommonSnapshot.Id)
-						if e != nil {
-							return &Event{
-								Name: "restrict-snapshots-error",
-								Args: &EventArgs{"err": e, "filesystemId": toFilesystemId},
-							}, backoffState
-						}
-						snapRange, e = canApply(localSnaps, remoteSnaps)
-						if err != nil {
-							return &Event{
-								Name: "failed-canapply-after-stash",
-								Args: &EventArgs{"err": e},
-							}, backoffState
+					}
+				case *ToSnapsAhead:
+					if transferRequest.StashDivergence {
+						event, state := stash(toFilesystemId, err.latestCommonSnapshot.Id, client, ctx)
+						if event != nil {
+							return event, state
 						}
 					}
 				}
-				if returnErr {
-					return &Event{
-						Name: "error-in-canapply-when-pushing", Args: &EventArgs{"err": err},
-					}, backoffState
-				}
+				return &Event{
+					Name: "error-in-canapply-when-pushing", Args: &EventArgs{"err": err},
+				}, backoffState
+
 			}
 			// TODO peer may error out of pushPeerState, wouldn't we like to get them
 			// back into it somehow? we could attempt to do that with by sending a new
