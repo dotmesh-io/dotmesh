b6bf35693b34c0301b84e43cf46f0cbff2221e86
Karolis Rusenas <karolis.rusenas@gmail.com>
2018-12-18 14:16:47 +0000

    #605 checking whether filesystem exists in etcd before moving forward

--- pkg/fsm/fsm_discovering.go
+++ pkg/fsm/fsm_discovering.go
@@ -1,17 +1,41 @@
 package fsm
 
 import (
-	"log"
+	"fmt"
+
+	"github.com/coreos/etcd/client"
+	"github.com/dotmesh-io/dotmesh/pkg/types"
+
+	"golang.org/x/net/context"
+
+	log "github.com/sirupsen/logrus"
 )
 
 func discoveringState(f *FsMachine) StateFn {
 	f.transitionedTo("discovering", "loading")
 	log.Printf("entering discovering state for %s", f.filesystemId)
 
-	err := f.discover()
-	if err != nil {
-		log.Printf("got error '%v' while discovering state", err)
+	// checking whether filesystem exists in the kv store
+	_, err := f.etcdClient.Get(
+		context.Background(),
+		fmt.Sprintf("%s/filesystems/masters/%s", types.EtcdPrefix, f.filesystemId),
+		&client.GetOptions{},
+	)
+	if err != nil && client.IsKeyNotFound(err) {
+		log.WithFields(log.Fields{
+			"error":         err,
+			"filesystem_id": f.filesystemId,
+		}).Warn("[discoveringState] filesystem doesn't exist in the KV store, entering failed state forever")
 		return failedState
+	}
+
+	err = f.discover()
+	if err != nil {
+		log.WithFields(log.Fields{
+			"error":         err,
+			"filesystem_id": f.filesystemId,
+		}).Error("[discoveringState] got error while discovering state")
+		return missingState
 	}
 
 	if !f.filesystem.Exists {
@@ -19,12 +43,11 @@ func discoveringState(f *FsMachine) StateFn {
 	} else {
 		err := f.state.AlignMountStateWithMasters(f.filesystemId)
 		if err != nil {
-			log.Printf(
-				"[discoveringState:%s] error trying to align mount state with masters: %v, "+
-					"going into failed state forever",
-				f.filesystemId,
-				err,
-			)
+			log.WithFields(log.Fields{
+				"error":         err,
+				"filesystem_id": f.filesystemId,
+			}).Error("[discoveringState] error trying to align mount state with masters, going into failed state forever")
+
 			return failedState
 		}
 		if f.filesystem.Mounted {
