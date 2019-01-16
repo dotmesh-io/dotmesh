044a5600bdd50cf54e0a0c896ba3c910a181de7d
Alaric Snell-Pym <alaric@dotmesh.com>
2019-01-16 10:16:39 +0000

    FIX: Reinstate `FQ` function

--- pkg/zfs/fs_util.go
+++ pkg/zfs/fs_util.go
@@ -6,6 +6,11 @@ import (
 	"github.com/dotmesh-io/dotmesh/pkg/types"
 )
 
+// FQ - from filesystem id to a fully qualified ZFS filesystem
+func FQ(poolName, fs string) string {
+	return fmt.Sprintf("%s/%s/%s", poolName, types.RootFS, fs)
+}
+
 // UnFQ - from fully qualified ZFS name to filesystem id, strip off prefix
 func UnFQ(poolName, fqfs string) string {
 	return fqfs[len(poolName+"/"+types.RootFS+"/"):]
