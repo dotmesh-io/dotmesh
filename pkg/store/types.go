4b576fc071884ce5f65defd27a91b771d3969f9c
Karolis Rusenas <karolis.rusenas@gmail.com>
2019-01-17 16:28:44 +0000

    #613 kv store types

--- pkg/store/types.go
+++ pkg/store/types.go
@@ -5,4 +5,13 @@ const (
 	FilesystemDeletedPrefix        = "/filesystems/deleted/"
 	FilesystemCleanupPendingPrefix = "/filesystems/cleanupPending/"
 	FilesystemLivePrefix           = "/filesystems/live/"
+	FilesystemContainersPrefix     = "/filesystems/containers/"
+)
+
+type KVType string
+
+const (
+	KVTypeEtcdV3 KVType = "etcdv3"
+	KVTypeMem    KVType = "mem"
+	KVTypeBolt   KVType = "bolt"
 )
