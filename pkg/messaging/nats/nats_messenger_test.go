17d1edf08e27f627b7e79cbd3d47ba624d440a49
Karolis Rusenas <karolis.rusenas@gmail.com>
2019-01-24 15:18:42 +0000

    moving to different port for test

--- pkg/messaging/nats/nats_messenger_test.go
+++ pkg/messaging/nats/nats_messenger_test.go
@@ -9,6 +9,9 @@ import (
 func TestServerStart(t *testing.T) {
 
 	cfg := DefaultConfig()
+	cfg.HTTPPort = 35600
+	cfg.ClusterPort = 35601
+	cfg.Port = 35602
 
 	srv, err := NewServer(cfg)
 	if err != nil {
@@ -27,6 +30,9 @@ func TestServerStartAndConnect(t *testing.T) {
 	log.SetLevel(log.DebugLevel)
 
 	cfg := DefaultConfig()
+	cfg.HTTPPort = 35603
+	cfg.ClusterPort = 35604
+	cfg.Port = 35605
 
 	srv, err := NewServer(cfg)
 	if err != nil {
@@ -40,7 +46,7 @@ func TestServerStartAndConnect(t *testing.T) {
 	defer srv.Shutdown()
 
 	client, err := NewClient(&ClientConfig{
-		NatsURL:           "nats://127.0.0.1:32609",
+		NatsURL:           "nats://127.0.0.1:35605",
 		ConnectionTimeout: 5,
 	})
 	if err != nil {
