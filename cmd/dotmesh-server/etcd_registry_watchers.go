550c8f31e61f268c1da725ba3e216d7becdc5dc8
Karolis Rusenas <karolis.rusenas@gmail.com>
2019-01-24 13:27:28 +0000

    #613 registry watchers

---
+++ cmd/dotmesh-server/etcd_registry_watchers.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/dotmesh-io/dotmesh/pkg/types"
+)
+
+func (s *InMemoryState) watchRegistryFilesystems() error {
+	vals, err := s.registryStore.ListFilesystems()
+	if err != nil {
+		return fmt.Errorf("failed to list server addresses: %s", err)
+	}
+
+	var idxMax uint64
+	for _, val := range vals {
+		if val.Meta.ModifiedIndex > idxMax {
+			idxMax = val.Meta.ModifiedIndex
+		}
+		s.processRegistryFilesystem(val)
+
+	}
+
+	return s.registryStore.WatchFilesystems(idxMax, func(rf *types.RegistryFilesystem) error {
+		s.processRegistryFilesystem(rf)
+		return nil
+	})
+}
+
+func (s *InMemoryState) processRegistryFilesystem(rf *types.RegistryFilesystem) error {
+	vn := types.VolumeName{
+		Namespace: rf.OwnerId,
+		Name:      rf.Name,
+	}
+	switch rf.Meta.Action {
+	case types.KVDelete:
+		return s.registry.UpdateFilesystemFromEtcd(vn, types.RegistryFilesystem{})
+	case types.KVCreate, types.KVSet:
+		return s.registry.UpdateFilesystemFromEtcd(vn, *rf)
+	default:
+		return nil
+	}
+}
+
+func (s *InMemoryState) watchRegistryClones() error {
+	vals, err := s.registryStore.ListClones()
+	if err != nil {
+		return fmt.Errorf("failed to list server addresses: %s", err)
+	}
+
+	var idxMax uint64
+	for _, val := range vals {
+		if val.Meta.ModifiedIndex > idxMax {
+			idxMax = val.Meta.ModifiedIndex
+		}
+		s.processRegistryClone(val)
+
+	}
+
+	return s.registryStore.WatchClones(idxMax, func(val *types.Clone) error {
+		return s.processRegistryClone(val)
+	})
+}
+
+func (s *InMemoryState) processRegistryClone(c *types.Clone) error {
+	switch c.Meta.Action {
+	case types.KVDelete:
+		s.registry.DeleteCloneFromEtcd(c.Name, c.FilesystemId)
+	case types.KVCreate, types.KVSet:
+		s.registry.UpdateCloneFromEtcd(c.Name, c.FilesystemId, *c)
+	}
+	return nil
+}
