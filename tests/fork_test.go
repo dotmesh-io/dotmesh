9a1b74315984920929f1d4e31d09874ebcee9cd3
Karolis Rusenas <karolis.rusenas@gmail.com>
2019-02-20 16:40:46 +0000

    deleting a fork, then forking again test

--- tests/fork_test.go
+++ tests/fork_test.go
@@ -132,5 +132,111 @@ func TestForks(t *testing.T) {
 		if getResp.ForkParentId != lookupResp {
 			t.Errorf("Expected fork parent %s, got %s", lookupResp, getResp.ForkParentId)
 		}
+
+		// Alice deletes the fork
+		citools.RunOnNode(t, node1Name, "dm remote switch alice")
+		citools.RunOnNode(t, node1Name, "dm dot delete -f alice/"+fsname2)
+	})
+
+	t.Run("DeleteThenForkAgain", func(t *testing.T) {
+		fsname := citools.UniqName()
+
+		// Bob makes a dot
+		citools.RunOnNode(t, node1Name, "dm remote switch bob")
+		citools.RunOnNode(t, node1Name, "dm init bob/"+fsname)
+		citools.RunOnNode(t, node1Name, "dm switch bob/"+fsname)
+		citools.RunOnNode(t, node1Name, "dm commit -m 'Nice Commit'")
+
+		// Admin forks the dot
+		fsname2 := citools.UniqName()
+
+		var lookupResp string
+		err = citools.DoRPC(f[0].GetNode(0).IP, "bob", bobKey,
+			"DotmeshRPC.Lookup",
+			VolumeName{Namespace: "bob", Name: fsname},
+			&lookupResp)
+		if err != nil {
+			t.Error(err)
+		}
+		forkResp := ""
+		err = citools.DoRPC(f[0].GetNode(0).IP, "admin", f[0].GetNode(0).ApiKey,
+			"DotmeshRPC.Fork",
+			ForkRequest{
+				MasterBranchID: lookupResp,
+				ForkNamespace:  "alice",
+				ForkName:       fsname2,
+			},
+			&forkResp)
+		if err != nil {
+			t.Error(err)
+		}
+
+		// Alice looks for it
+		citools.RunOnNode(t, node1Name, "dm remote switch alice")
+		output := citools.OutputFromRunOnNode(t, node1Name, "dm list")
+		if !strings.Contains(output, fsname2) {
+			t.Errorf("Did not find dot %s in output. Got: %s", fsname2, output)
+		}
+		citools.RunOnNode(t, node1Name, "dm switch alice/"+fsname2)
+		output = citools.OutputFromRunOnNode(t, node1Name, "dm log")
+		if !strings.Contains(output, "Nice Commit") {
+			t.Errorf("Did not find commit in output. Expected '%s', got: '%s'", "Nice Commit", output)
+		}
+
+		// Check List returns appropriate metadata
+		var listResp map[string]map[string]types.DotmeshVolume
+		err = citools.DoRPC(f[0].GetNode(0).IP, "alice", aliceKey,
+			"DotmeshRPC.List",
+			VolumeName{Namespace: "alice", Name: fsname2},
+			&listResp)
+		if err != nil {
+			t.Error(err)
+		}
+		if listResp["alice"][fsname2].ForkParentId != lookupResp {
+			t.Errorf("Expected fork parent %s, got %s", lookupResp, listResp["alice"][fsname2].ForkParentId)
+		}
+
+		// Check Get returns appropriate metadata
+		var getResp types.DotmeshVolume
+		err = citools.DoRPC(f[0].GetNode(0).IP, "alice", aliceKey,
+			"DotmeshRPC.Get",
+			listResp["alice"][fsname2].Id,
+			&getResp)
+		if err != nil {
+			t.Error(err)
+		}
+		if getResp.ForkParentId != lookupResp {
+			t.Errorf("Expected fork parent %s, got %s", lookupResp, getResp.ForkParentId)
+		}
+
+		// Alice deletes her fork (didn't like the color)
+		citools.RunOnNode(t, node1Name, "dm remote switch alice")
+		citools.RunOnNode(t, node1Name, "dm dot delete -f alice/"+fsname2)
+
+		// fork again
+		secondForkResp := ""
+		err = citools.DoRPC(f[0].GetNode(0).IP, "admin", f[0].GetNode(0).ApiKey,
+			"DotmeshRPC.Fork",
+			ForkRequest{
+				MasterBranchID: lookupResp,
+				ForkNamespace:  "alice",
+				ForkName:       fsname2,
+			},
+			&secondForkResp)
+		if err != nil {
+			t.Error(err)
+		}
+
+		// Alice looks for it
+		citools.RunOnNode(t, node1Name, "dm remote switch alice")
+		secondOutput := citools.OutputFromRunOnNode(t, node1Name, "dm list")
+		if !strings.Contains(secondOutput, fsname2) {
+			t.Errorf("Did not find dot %s in output. Got: %s", fsname2, secondOutput)
+		}
+		citools.RunOnNode(t, node1Name, "dm switch alice/"+fsname2)
+		secondOutput = citools.OutputFromRunOnNode(t, node1Name, "dm log")
+		if !strings.Contains(secondOutput, "Nice Commit") {
+			t.Errorf("Did not find commit in output. Expected '%s', got: '%s'", "Nice Commit", secondOutput)
+		}
 	})
 }
