0d83e26edd202964fe74b3e576ab21425a34d2f8
Karolis Rusenas <karolis.rusenas@gmail.com>
2019-03-21 17:34:17 +0000

    boltdb config

--- pkg/types/kv_store.go
+++ pkg/types/kv_store.go
@@ -4,3 +4,14 @@ const DefaultEtcdURL = "https://dotmesh-etcd:42379"
 const DefaultEtcdClientPort = "42379"
 
 const EnvEtcdEndpoint = "DOTMESH_ETCD_ENDPOINT"
+const EnvStorageBackend = "DOTMESH_STORAGE"
+const EnvDotmeshBoltdbPath = "DOTMESH_BOLTDB_PATH"
+
+const (
+	StorageBackendEtcd   = "etcd"
+	StorageBackendBoltdb = "boltdb"
+)
+
+const (
+	DefaultBoltdbPath = "/data"
+)
