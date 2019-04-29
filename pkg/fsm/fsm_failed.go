af2113e454c00eac4e742d0cff04c0753f7200de
Alaric Snell-Pym <alaric@dotmesh.com>
2019-04-29 13:42:41 +0100

    #673: Make the failed state reject requests

--- pkg/fsm/fsm_failed.go
+++ pkg/fsm/fsm_failed.go
@@ -1,11 +1,21 @@
 package fsm
 
 import (
+	"github.com/dotmesh-io/dotmesh/pkg/types"
+
 	log "github.com/sirupsen/logrus"
 )
 
 func failedState(f *FsMachine) StateFn {
 	f.transitionedTo("failed", "never coming back")
 	log.Infof("entering failed state for %s", f.filesystemId)
-	select {}
+	for {
+		select {
+		case _ = <-f.innerRequests:
+			f.innerResponses <- &types.Event{
+				Name: "failed",
+				Args: &types.EventArgs{},
+			}
+		}
+	}
 }
