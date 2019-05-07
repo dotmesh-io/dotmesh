2b9ea1a84e84166f897bfabe39e12ebb3238a372
Karolis Rusenas <karolis.rusenas@gmail.com>
2019-05-07 16:40:51 +0100

    encode/decode using base64+gob

--- pkg/types/diff.go
+++ pkg/types/diff.go
@@ -1,6 +1,13 @@
 package types
 
-type FileChange int
+import (
+	"bytes"
+	"encoding/base64"
+	"encoding/gob"
+	"fmt"
+)
+
+type FileChange uint
 
 const (
 	FileChangeUnknown FileChange = iota
@@ -10,9 +17,46 @@ const (
 	FileChangeRenamed
 )
 
+func (c FileChange) String() string {
+	switch c {
+	case FileChangeAdded:
+		return "+"
+	case FileChangeModified:
+		return "M"
+	case FileChangeRemoved:
+		return "-"
+	case FileChangeRenamed:
+		return "R"
+	default:
+		return "unknown"
+	}
+}
+
 type ZFSFileDiff struct {
-	Change   FileChange
-	Filename string
+	Change   FileChange `json:"change"`
+	Filename string     `json:"filename"`
+}
+
+func EncodeZFSFileDiff(files []ZFSFileDiff) (string, error) {
+	buf := bytes.Buffer{}
+	enc := gob.NewEncoder(&buf)
+	err := enc.Encode(&files)
+	if err != nil {
+		return "", nil
+	}
+	return base64.StdEncoding.EncodeToString(buf.Bytes()), nil
+}
+
+func DecodeZFSFileDiff(data string) ([]ZFSFileDiff, error) {
+
+	decoded, err := base64.StdEncoding.DecodeString(data)
+	if err != nil {
+		return nil, fmt.Errorf("failed base64 decode step: %s", err)
+	}
+
+	var files []ZFSFileDiff
+	err = gob.NewDecoder(bytes.NewBuffer(decoded)).Decode(&files)
+	return files, err
 }
 
 type RPCDiffRequest struct {
