70bae3c2b5c912043521462d052fb084dc5873f3
Karolis Rusenas <karolis.rusenas@gmail.com>
2019-05-29 13:07:01 +0100

    write, rollback, log

--- pkg/fsm/fsm_active_file_io.go
+++ pkg/fsm/fsm_active_file_io.go
@@ -5,6 +5,7 @@ import (
 	"io"
 	"os"
 	"path/filepath"
+	"strings"
 
 	"github.com/dotmesh-io/dotmesh/pkg/archiver"
 	"github.com/dotmesh-io/dotmesh/pkg/types"
@@ -14,41 +15,75 @@ import (
 )
 
 func (f *FsMachine) saveFile(file *types.InputFile) StateFn {
+	logger := log.WithFields(log.Fields{
+		"filesystem_id": f.filesystemId,
+		"filename":      file.Filename,
+		"user":          file.User,
+	})
 	// create the default paths
-
 	destPath := filepath.Join(utils.Mnt(f.filesystemId), "__default__", file.Filename)
 	log.Printf("Saving file to %s", destPath)
 
-	err := os.Rename(file.Filepath, destPath)
+	directoryPath := destPath[:strings.LastIndex(destPath, "/")]
+	err := os.MkdirAll(directoryPath, 0775)
 	if err != nil {
-		file.Response <- &types.Event{
-			Name: types.EventNameSaveFailed,
-			Args: &types.EventArgs{"err": fmt.Errorf("failed to create directory, error: %s", err)},
-		}
+		logger.WithError(err).Errorf("failed to create directory for file upload")
+		file.Response <- types.NewErrorEvent(types.EventNameSaveFailed, fmt.Errorf("failed to create directory, error: %s", err))
+		return backoffState
+	}
+	logger.Info("creating file")
+	out, err := os.Create(destPath)
+	if err != nil {
+		logger.WithError(err).Errorf("failed to create file for file upload")
+		file.Response <- types.NewErrorEvent(types.EventNameSaveFailed, fmt.Errorf("failed to create file, error: %s", err))
 		return backoffState
 	}
 
+	logger.Info("writing to file")
+
+	defer func() {
+		err := out.Close()
+		if err != nil {
+			log.WithFields(log.Fields{
+				"error": err,
+				"file":  destPath,
+			}).Error("s3 saveFile: got error while closing output file")
+		}
+	}()
+
+	bytes, err := io.Copy(out, file.Contents)
+	if err != nil {
+		// if write failed - deleting it
+		removeErr := os.Remove(destPath)
+		if removeErr != nil {
+			logger.WithFields(log.Fields{
+				"error":       removeErr,
+				"write_error": err,
+			}).Warnf("file cleanup failed after unsuccessful upload")
+		}
+		logger.WithError(err).Errorf("failed to write file contents")
+		file.Response <- types.NewErrorEvent(types.EventNameSaveFailed, fmt.Errorf("cannot to create a file, error: %s", err))
+		return backoffState
+	}
+	logger.WithFields(log.Fields{
+		"file": destPath,
+	}).Info("file write done")
 	response, _ := f.snapshot(&types.Event{Name: "snapshot",
 		Args: &types.EventArgs{"metadata": map[string]string{
-			"message":      "Uploaded " + file.Filename + " (" + formatBytes(file.Size) + ")",
+			"message":      "Uploaded " + file.Filename + " (" + formatBytes(bytes) + ")",
 			"author":       file.User,
 			"type":         "upload",
 			"upload.type":  "S3",
 			"upload.file":  file.Filename,
-			"upload.bytes": fmt.Sprintf("%d", file.Size),
+			"upload.bytes": fmt.Sprintf("%d", bytes),
 		}}})
 	if response.Name != "snapshotted" {
-		file.Response <- &types.Event{
-			Name: types.EventNameSaveFailed,
-			Args: &types.EventArgs{"err": "file snapshot failed"},
-		}
+		logger.WithError(err).Errorf("filesystem snapshot failed after file upload")
+		file.Response <- types.NewErrorEvent(types.EventNameSaveFailed, fmt.Errorf("snapshot failed"))
 		return backoffState
 	}
 
-	file.Response <- &types.Event{
-		Name: types.EventNameSaveSuccess,
-		Args: &types.EventArgs{},
-	}
+	file.Response <- types.NewEvent(types.EventNameSaveSuccess)
 
 	return activeState
 }
