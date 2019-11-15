4d881f07799dd4187f5831d40be27af412cacd91
Itamar Turner-Trauring <itamar@dotscience.com>
2019-11-15 10:38:26 -0500

    #766: Infrastructure for writing ZFS tests.

--- pkg/zfs/zfs_test.go
+++ pkg/zfs/zfs_test.go
@@ -2,8 +2,12 @@ package zfs
 
 import (
 	"fmt"
+	"os"
+	"os/exec"
 	"testing"
 	"time"
+
+	"github.com/dotmesh-io/dotmesh/pkg/uuid"
 )
 
 var out = `CREATION
@@ -32,28 +36,84 @@ func TestParseCreationOutput(t *testing.T) {
 	}
 }
 
-func TestZFSDiffCaching(t *testing.T) {
-	// TODO presupposes pool named test was already created...
-	z, err := NewZFS("zfs", "zpool", "test", "test")
+type cleanupFunc func()
+
+// Run command with stdout and stderr printed.
+func verboseRun(program string, args ...string) error {
+	cmd := exec.Command(program, args...)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	return cmd.Run()
+}
+
+// Fail test if commands fails.
+func mustRun(t *testing.T, program string, args ...string) {
+	err := verboseRun(program, args...)
+	if err != nil {
+		t.Fatalf("Failed to run %s %#v: %s", program, args, err)
+	}
+}
+
+func createPoolAndFilesystem(t *testing.T) (z ZFS, fsName, fsMountPath string, cleanup cleanupFunc) {
+	// Delete any pre-existing pool-id.
+	os.Remove("/dotmesh-pool-id")
+
+	// Create the pool
+	poolName := "fs" + uuid.New().String()
+	wd, err := os.Getwd()
+	if err != nil {
+		t.Fatalf("Can't get working directory!? %s", err)
+	}
+	poolPath := wd + "/" + poolName + ".zpool"
+	mustRun(t, "truncate", "-s", "100M", poolPath)
+	mustRun(t, "zpool", "create", poolName, poolPath)
+	fsName = "mytestfs"
+	fsMountPath = "/" + poolName + "/dmfs/" + fsName + "/"
+
+	// Create the zfs object
+	z, err = NewZFS("zfs", "zpool", poolName, poolName)
 	if err != nil || z == nil {
 		t.Fatalf("Error creating pool: %s", err)
 	}
+
+	// Create the dmfs filesystem structure
 	z.FindFilesystemIdsOnSystem()
+
+	// Create the filesystem
 	output, err := z.Create("mytestfs")
 	if err != nil {
 		t.Fatalf("Error creating fs: %s\n%s", err, output)
 	}
-	defer z.DeleteFilesystemInZFS("mytestfs")
-	output, err = z.Snapshot("mytestfs", "myfirstsnapshot", []string{})
+
+	cleanup = func() {
+		// If env variable is set, don't destroy the pool at end of test, for easier
+		// debugging.
+		if os.Getenv("ZFS_TEST_DEBUG") == "" {
+			mustRun(t, "zpool", "destroy", poolName)
+			os.Remove(poolPath)
+		}
+	}
+	return z, fsName, fsMountPath, cleanup
+}
+
+func TestZFSDiffCaching(t *testing.T) {
+	z, fsName, _, cleanup := createPoolAndFilesystem(t)
+	defer cleanup()
+
+	output, err := z.Snapshot(fsName, "myfirstsnapshot", []string{})
 	if err != nil {
 		t.Fatalf("Error snapshotting: %s\n%s", err, output)
 	}
 	fmt.Printf("DIFF TIME!\n")
-	_, err = z.Diff("mytestfs")
+	_, err = z.Diff(fsName)
 	if err != nil {
 		t.Fatalf("Error diffing: %s\n", err)
 	}
-	_, err = z.Diff("mytestfs")
+	_, err = z.Diff(fsName)
+	if err != nil {
+		t.Fatalf("Error diffing second time: %s\n", err)
+	}
+	_, err = z.Diff(fsName)
 	if err != nil {
 		t.Fatalf("Error diffing second time: %s\n", err)
 	}
